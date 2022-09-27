0c2905f8af3aad69c058a221fa97d1aa812163bd
cdperez <cdperez.85@gmail.com>
2022-09-27 19:32:11 +0200

    actualizacion ejercicio variables

--- Javascript/01-variable/app.js
+++ Javascript/01-variable/app.js
@@ -3,3 +3,16 @@ var userInfo = {
     surname: 'Díaz'
 }
 
+let nameWomen = 'Miriapodo'
+function showName () {
+    let name = 'Pepe';
+    console.log(name)
+}
+
+function showOtherNames() {
+    // console.log(name)
+    console.log(nameWomen)
+}
+
+showName();
+showOtherNames();
