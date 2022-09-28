95bc53753d5c0d910b862ea69c109dbce5a47da3
cdperez <cdperez.85@gmail.com>
2022-09-29 00:11:22 +0200

    Iteration#4

--- Javascript/neoland-basic-algorithms/app.js
+++ Javascript/neoland-basic-algorithms/app.js
@@ -11,6 +11,7 @@ z = h+y;
 const character = {name: 'Jack Sparrow', age: 10};
 character.age = 25;
 
+
 // 1.2
 firstName = 'Jon'; 
 lastName = 'Snow'; 
@@ -32,3 +33,31 @@ const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 globalBasePrice = 25000;
 car1.finalPrice+=globalBasePrice;
 car2.finalPrice+=globalBasePrice;
+
+// Iteración 3
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+console.log(10*5);
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+console.log(10/2);
+// 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+console.log(10%5);
+// 1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+// teniendo dos variables p = 10 y j = 5.
+let p = 10;
+let j = 5;
+let o = p + j;
+
+// 1.5 Usa el correcto operador de asignación que resultará en i = 50,
+// teniendo dos variables c = 10 y m = 5.
+let c = 10;
+let m = 5;
+let i = c * j;
+
+// Iteración 4
+// 1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers[0]);
+
+avengers[0]="IRONMAN";
+console.log(avengers.length);
