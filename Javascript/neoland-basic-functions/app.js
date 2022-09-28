541fa8a9d11f6d2fcecdfe93d2798256c7c81669
cdperez <cdperez.85@gmail.com>
2022-09-29 00:47:20 +0200

    Iteration#2

--- Javascript/neoland-basic-functions/app.js
+++ Javascript/neoland-basic-functions/app.js
@@ -6,3 +6,22 @@ function sum(numberOne, numberTwo) {
     else 
         return numberTwo
 }
+
+// Iteration 2
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+function findLongestWord(a) {
+
+    let palabraMasLarga='';
+
+    for (let i=0;i<a.length;i++)
+        if (a[i].length > palabraMasLarga.length)
+            palabraMasLarga = a[i];
+        
+    return palabraMasLarga
+
+
+}
+
+console.log(findLongestWord(avengers))
