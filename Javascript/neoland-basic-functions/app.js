e8466419b16a23b17a4794d94320446e8e3553e0
cdperez <cdperez.85@gmail.com>
2022-09-29 02:02:08 +0200

    Iteracion#6

--- Javascript/neoland-basic-functions/app.js
+++ Javascript/neoland-basic-functions/app.js
@@ -51,8 +51,6 @@ function average(a) {
     return suma/a.length;
 }
 
-console.log(average(numbers));
-
 // Iterar 5
 const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
 function averageWord(a) {
@@ -68,4 +66,48 @@ function averageWord(a) {
 
 }
 
-console.log(averageWord(mixedElements));
+
+// Iterar 6
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda',
+    'pizza'
+];
+
+
+// Propongo dos soluciones:
+//  Solución 1 // Usando un tipo SET
+
+function removeDuplicates_1(a) {
+    let arraySet = new Set(a);
+
+    return Array.from(arraySet)
+
+}
+
+// Solución 2 // Comparador de strings
+
+function removeDuplicates_2(a){
+    let comparador = '';
+
+    for (let i=0;i<a.length;i++) {
+        comparador = a[i];
+        for (let j= i+1;j<a.length;j++)
+            if(comparador===a[j])
+            {                
+                console.log(a[j])
+                a.splice(j,1);
+            }
+        }
+
+    return a;
+}
